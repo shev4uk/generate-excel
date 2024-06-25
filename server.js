117f50f5057583aebe93dc04f907b4cd24d664f5
shev4uk <shev4ukyuri@gmail.com>
2024-06-25 17:45:41 -0500

    Update server.js

--- server.js
+++ server.js
@@ -44,25 +44,24 @@ app.post('/generate-excel', async (req, res) => {
     numberFormat: '#,##0.00; (#,##0.00); -',
   });
 
-  // Write column headers
-  const headers = Object.keys(jsonData[0]);
-  headers.forEach((header, index) => {
-    ws.cell(1, index + 1)
-      .string(header.charAt(0).toUpperCase() + header.slice(1))
-      .style(headerStyle);
-  });
+  const {headers, rowsData} = jsonData;
 
-  // Write data
-  jsonData.forEach((data, rowIndex) => {
-    headers.forEach((header, colIndex) => {
-      const cellValue = data[header];
-      if (typeof cellValue === 'number') {
-        ws.cell(rowIndex + 2, colIndex + 1).number(cellValue).style(cellStyle);
-      } else {
-        ws.cell(rowIndex + 2, colIndex + 1).string(cellValue).style(cellStyle);
-      }
+const columnNames = Object.values(headers);
+    const cellKeys = Object.keys(headers);
+    columnNames.forEach((header, index) => {
+      ws.cell(1, index + 1).string(header);
     });
-  });
+
+    rowsData.forEach((data, rowIndex) => {
+      cellKeys.forEach((key, colIndex) => {
+        const cellValue = data[key];
+        if (typeof cellValue === 'number') {
+          ws.cell(rowIndex + 2, colIndex + 1).number(cellValue);
+        } else {
+          ws.cell(rowIndex + 2, colIndex + 1).string(cellValue);
+        }
+      });
+    });
 
   const buffer = await wb.writeToBuffer();
   const base64Buffer = buffer.toString('base64');
